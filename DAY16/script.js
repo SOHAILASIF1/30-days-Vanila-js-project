400a4fdfd1a1a396d1e86823d436d6e0618f1070
SOHAILASIF1 <alyana998877@gmail.com>
2026-02-13 23:41:00 +0500

    hjik

--- DAY16/script.js
+++ DAY16/script.js
@@ -1,11 +1,20 @@
-let today = new Date()
-// This is Dec 1, 2028
-let remaining = new Date(2028, 11, 1); 
+//countdown app
 
-let ans=remaining-today
-let remaingtime=new Date(ans)
- let yearR=remaingtime.getFullYear()
-
-
-console.log(yearR);
+let today=new Date();
+let currentYear=today.getFullYear();
+let newYear=new Date(`January 1 ${currentYear+1} 00:00:00`);    
+let sec=1000;
+let min=sec*60;
+let hour=min*60;
+let day=hour*24;    
+function updateCountdown(){
+    let currentTime=new Date();
+    let diff=newYear-currentTime;
+    let d=Math.floor(diff/day);
+    let h=Math.floor((diff%day)/hour);
+    let m=Math.floor((diff%hour)/min);
+    let s=Math.floor((diff%min)/sec);
+    document.getElementById("countdown").innerHTML=`${d}d ${h}h ${m}m ${s}s`;
+}
+setInterval(updateCountdown,1000);  
 
